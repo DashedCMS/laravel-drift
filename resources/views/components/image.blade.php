0566a6a14778ce57448ca8f314cc66eeefd48583
Robin van Maasakker <robin@qubiqx.com>
2024-06-03 22:04:34 +0200

    Update

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -2,18 +2,34 @@
     'config',
     'path',
     'manipulations' => [],
+    'alt' => '',
+    'loading' => Customsetting::get('image_force_lazy_load', null, false) ? 'lazy' : 'eager',
+    'mediaId',
+    'conversion' => 'medium',
+    'class' => ''
 ])
-@php
-    $url = app(\Dashed\Drift\UrlBuilder::class)->url($config, $path, $manipulations);
-    $lazyLoad = app(\Dashed\Drift\DriftManager::class)->configs()->where('name', $config)->first()->forceLazyLoad;
-    $showSizes = app(\Dashed\Drift\DriftManager::class)->configs()->where('name', $config)->first()->showSizes;
-    if($showSizes){
-        $size = app(\Dashed\Drift\UrlBuilder::class)->size($url);
-    }
-@endphp
-<img
-        src="{{ $url }}"
-        {{ $attributes }}
-        @if($lazyLoad && !array_key_exists('loading', collect($attributes)->toArray())) loading="lazy" @endif
-        @if($size ?? false) width="{{ $size['width'] }}" height="{{ $size['height'] }}" @endif
->
+@if((int)$path == $path)
+    <x-dashed-files::image
+            class="{{ $class }}"
+            loading="{{ $loading }}"
+            :mediaId="$path"
+            conversion="{{ $conversion }}"
+    />
+@else
+    @php
+        $url = app(\Dashed\Drift\UrlBuilder::class)->url($config, $path, $manipulations);
+        $lazyLoad = app(\Dashed\Drift\DriftManager::class)->configs()->where('name', $config)->first()->forceLazyLoad;
+        $showSizes = app(\Dashed\Drift\DriftManager::class)->configs()->where('name', $config)->first()->showSizes;
+        if($showSizes){
+            $size = app(\Dashed\Drift\UrlBuilder::class)->size($url);
+        }
+    @endphp
+    <img
+            class="{{ $class }}"
+            alt="{{ $alt }}"
+            src="{{ $url }}"
+            {{ $attributes }}
+            @if($lazyLoad && !array_key_exists('loading', collect($attributes)->toArray())) loading="lazy" @endif
+            @if($size ?? false) width="{{ $size['width'] }}" height="{{ $size['height'] }}" @endif
+    >
+@endif
